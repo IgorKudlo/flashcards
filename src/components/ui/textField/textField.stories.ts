a26afae1a99a548726dfa2c369d938937dd2d724
igor.kudlo <igoreklida@gmail.com>
2024-03-14 11:31:04 +0300

    TextField

---
+++ src/components/ui/textField/textField.stories.ts
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TextField } from './textField';
+
+const meta = {
+  title: 'Components/TextField',
+  component: TextField,
+  tags: ['autodocs']
+} satisfies Meta<typeof TextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    placeholder: 'Input',
+    label: 'Input'
+  }
+};
+
+export const Password: Story = {
+  args: {
+    placeholder: 'Input',
+    label: 'Input',
+    variant: 'password'
+  }
+};
+
+export const Search: Story = {
+  args: {
+    placeholder: 'Input search',
+    variant: 'search'
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    placeholder: 'Input',
+    label: 'Input',
+    disabled: true
+  }
+};
+
+export const Error: Story = {
+  args: {
+    placeholder: 'Input',
+    label: 'Input',
+    error: true,
+    errorMessage: 'Error!'
+  }
+};
---
+++ src/components/ui/textField/textField.tsx
@@ -0,0 +1,41 @@
+import s from './textField.module.scss';
+import eye from '@/assets/images/eye.svg';
+import search from '@/assets/images/search.svg';
+import { ComponentPropsWithoutRef } from 'react';
+import { Typography } from '@/components/ui/typography';
+
+export type TextFieldPros = {
+  label?: string;
+  variant?: 'password' | 'search';
+  disabled?: boolean;
+  error?: boolean;
+  errorMessage?: string;
+} & ComponentPropsWithoutRef<'input'>
+
+export const TextField = (props: TextFieldPros) => {
+  const { label, variant, className, disabled = false, placeholder, error = false, errorMessage, ...rest } = props;
+
+  const FieldPassword = () => (
+    <div className={s['password-field']}>
+      <input className={s.input} type='password' placeholder={placeholder} disabled={disabled} {...rest}/>
+      <img className={s.icon} src={eye} alt=""/>
+    </div>
+  );
+
+  const FieldSearch = () => (
+    <div className={s['search-field']}>
+      <img className={s.icon} src={search} alt=""/>
+      <input className={s.input} type='search' placeholder={placeholder} disabled={disabled} {...rest}/>
+    </div>
+  );
+
+  return (
+    <div className={`${className ?? ''} ${s['text-field']} ${disabled ? s.disabled : ''} ${error ? s.error : ''}`}>
+      {label && <Typography as={'label'} className={s.label}>{label}</Typography>}
+      {!variant && <input className={s.input} type='text' placeholder={placeholder} disabled={disabled} {...rest}/>}
+      {variant === 'password' && <FieldPassword/>}
+      {variant === 'search' && <FieldSearch/>}
+      {errorMessage && <span className={s['error-message']}>{errorMessage}</span>}
+    </div>
+  );
+};
