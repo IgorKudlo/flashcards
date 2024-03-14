488b6cc257b9017fef8dfd6d043fadec5af2c04a
igor.kudlo <igoreklida@gmail.com>
2024-03-14 20:38:57 +0300

    Radix Checkbox

---
+++ src/components/ui/checkbox/checkbox.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Checkbox } from './checkbox';
+
+const meta = {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs']
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {};
+
+export const Label: Story = {
+  args: {
+    label: 'Check-box'
+  }
+};
