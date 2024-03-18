c758e51af9831ea59b0528865faa0ebfbef9be93
igor.kudlo <igoreklida@gmail.com>
2024-03-18 10:28:05 +0300

    react hook form

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,25 +1,39 @@
 import authCard from '../auth-card.module.scss';
+import { useController, useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import { TextField } from '@/components/ui/text-field';
 import { Card } from '@/components/ui/card';
 import { Typography } from '@/components/ui/typography';
 import { Checkbox } from '@/components/ui/checkbox';
-import { useState } from 'react';
+
+type FormValues = {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+}
 
 export const LoginForm = () => {
-  const [isCheck, setIsCheck] = useState(false);
+  const { register, handleSubmit, control } = useForm<FormValues>();
 
-  const onChangeRememberMe = (checked: boolean) => {
-    setIsCheck(checked);
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
   };
+
+  const {
+    field: { value, onChange }
+  } = useController({
+    name: 'rememberMe',
+    control,
+    defaultValue: false
+  });
 
   return (
     <Card className={authCard.card}>
       <Typography as={'h1'} variant={'h1'} className={authCard.title}>Sign In</Typography>
-      <form>
-        <TextField label={'Email'} className={authCard.field} />
-        <TextField label={'Password'} variant={'password'} className={authCard.field} />
-        <Checkbox onChange={() => onChangeRememberMe} checked={isCheck} label={'Remember Me'} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField {...register('email')} label={'Email'} className={authCard.field} />
+        <TextField {...register('password')} label={'Password'} variant={'password'} className={authCard.field} />
+        <Checkbox onChange={onChange} checked={value} label={'Remember Me'} />
         <Typography variant={'body2'} className={authCard['right-text']}>Forgot Password?</Typography>
         <Button type="submit" fullWidth className={authCard.button}>Submit</Button>
         <Typography variant={'body2'} className={authCard['center-text']}>Don't have an account?</Typography>
--- src/components/ui/text-field/textField.tsx
+++ src/components/ui/text-field/textField.tsx
@@ -1,7 +1,7 @@
 import s from './textField.module.scss';
 import eye from '@/assets/images/eye.svg';
 import search from '@/assets/images/search.svg';
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
 import { Typography } from '@/components/ui/typography';
 
 export type TextFieldPros = {
@@ -12,12 +12,12 @@ export type TextFieldPros = {
   errorMessage?: string;
 } & ComponentPropsWithoutRef<'input'>
 
-export const TextField = (props: TextFieldPros) => {
+export const TextField = forwardRef<HTMLInputElement, TextFieldPros>((props, ref) => {
   const { label, variant, className, disabled = false, placeholder, error = false, errorMessage, ...rest } = props;
 
   const FieldPassword = () => (
     <div className={s['password-field']}>
-      <input className={s.input} type='password' placeholder={placeholder} disabled={disabled} {...rest}/>
+      <input className={s.input} type='password' placeholder={placeholder} disabled={disabled} ref={ref} {...rest}/>
       <img className={s.icon} src={eye} alt=""/>
     </div>
   );
@@ -25,17 +25,17 @@ export const TextField = (props: TextFieldPros) => {
   const FieldSearch = () => (
     <div className={s['search-field']}>
       <img className={s.icon} src={search} alt=""/>
-      <input className={s.input} type='search' placeholder={placeholder} disabled={disabled} {...rest}/>
+      <input className={s.input} type='search' placeholder={placeholder} disabled={disabled} ref={ref} {...rest}/>
     </div>
   );
 
   return (
     <div className={`${className ?? ''} ${s['text-field']} ${disabled ? s.disabled : ''} ${error ? s.error : ''}`}>
       {label && <Typography as={'label'} className={s.label}>{label}</Typography>}
-      {!variant && <input className={s.input} type='text' placeholder={placeholder} disabled={disabled} {...rest}/>}
+      {!variant && <input className={s.input} type='text' placeholder={placeholder} disabled={disabled} ref={ref} {...rest}/>}
       {variant === 'password' && <FieldPassword/>}
       {variant === 'search' && <FieldSearch/>}
       {errorMessage && <span className={s['error-message']}>{errorMessage}</span>}
     </div>
   );
-};
+});
