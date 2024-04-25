9ff8c685450f4def130634f893e54760f242d5e8
igor.kudlo <igoreklida@gmail.com>
2024-04-25 14:07:27 +0300

    register form

---
+++ src/components/auth/register-form/register-form.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { RegisterForm } from './register-form';
+
+const meta = {
+  title: 'Auth/RegisterForm',
+  component: RegisterForm,
+  tags: ['autodocs']
+} satisfies Meta<typeof RegisterForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {};
---
+++ src/components/auth/register-form/register-form.tsx
@@ -0,0 +1,61 @@
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Card, Input, Typography } from '@/components';
+import s from './register-form.module.scss';
+
+const registerSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+  confirmPassword: z.string().min(3)
+})
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords don\'t match',
+    path: ['confirmPassword']
+  });
+
+type FormValues = z.infer<typeof registerSchema>;
+
+export const RegisterForm = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
+    resolver: zodResolver(registerSchema)
+  });
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Typography variant={'h1'} className={s.header}>Sign Up</Typography>
+        <Input
+          {...register('email')}
+          label={'Email'}
+          className={s.email}
+          error={errors.email?.message}
+        />
+        <Input
+          {...register('password')}
+          label={'Password'}
+          type={'password'}
+          className={s.password}
+          error={errors.password?.message}
+        />
+        <Input
+          {...register('confirmPassword')}
+          label={'Confirm Password'}
+          type={'password'}
+          className={s.confirmPassword}
+          error={errors.confirmPassword?.message}
+        />
+        <Button fullWidth type="submit" className={s.action}>Sign Up</Button>
+        <div className={s.helpContent}>
+          <Typography variant={'body2'} className={s.helpContentText}>Already have an account?</Typography>
+          <Typography href={'#'} as={'a'} variant={'link1'}>Sign In</Typography>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
