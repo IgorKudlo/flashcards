bd284aaf68fe29413b5d21a1e6e5d2e4eb13d374
igor.kudlo <igoreklida@gmail.com>
2024-04-27 15:11:31 +0300

    react-router, private routes

---
+++ src/router.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, Navigate, Outlet, RouteObject, RouterProvider } from 'react-router-dom';
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <div>login</div>
+  }
+];
+
+const privateRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>hello</div>
+  }
+];
+
+function PrivateRoutes() {
+  const isAuthenticated = false;
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+}
+
+const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: privateRoutes
+  },
+  ...publicRoutes
+]);
+
+
+export const Router = () => {
+  return <RouterProvider router={router} />;
+};
